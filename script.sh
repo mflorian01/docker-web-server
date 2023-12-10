7ee3701b5b2b6582ce37718ad1115565382996d2
mflorian01 <42562792+mflorian01@users.noreply.github.com>
2023-12-09 21:59:53 -0400

    Update script.sh
    Development

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "<H1>Hello, webserver running on container: $(hostname)</H1>" > index.html
+echo "<H1>Hello, Development webserver running on container: $(hostname)</H1>" > index.html
 busybox httpd -f -v -p 3000
